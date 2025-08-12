3fafee0960f0a9bc4e2cc356759f736a5a542e9b
osama-khalel <osamabkh2006@gmail.com>
2025-08-12 21:07:08 +0300

    Add files via upload

---
+++ Problem_Solving.cpp
@@ -0,0 +1,72 @@
+/*
+                             uu$$$$$$$$$$$uu
+                          uu$$$$$$$$$$$$$$$$$uu
+                         u$$$$$$$$$$$$$$$$$$$$$u
+                         u$$$$$$$$$$$$$$$$$$$$$$$u
+                       u$$$$$$$$$$$$$$$$$$$$$$$$$u
+                       u$$$$$$$$$$$$$$$$$$$$$$$$$u
+                       u$$$$$$=   =$$$=   =$$$$$$u
+                       =$$$$=      u$u       $$$$=
+                        $$$u       u$u       u$$$
+                        $$$u      u$$$u      u$$$
+                         =$$$$uu$$$   $$$uu$$$$=
+                          =$$$$$$$=   =$$$$$$$=
+                            u$$$$$$$$$$$$$$$u
+                             u$=$=$=$=$=$=$u
+                  uuu        $$u$ $ $ $ $u$$       uuu
+                 u$$$$        $$$$$u$u$u$$$       u$$$$
+                  $$$$$uu      =$$$$$$$$$=     uu$$$$$$
+                u$$$$$$$$$$$uu    =====    uuuu$$$$$$$$$
+                $$$$===$$$$$$$$$$uuu   uu$$$$$$$$$===$$$=
+                 ===      ==$$$$$$$$$$$uu ==$===
+                          uuuu ==$$$$$$$$$$uuu
+                 u$$$uuu$$$$$$$$$uu ==$$$$$$$$$$$uuu$$$
+                 $$$$$$$$$$====           ==$$$$$$$$$$$=
+                   =$$$$$=                      ==$$$$==
+                     $$$=                         $$$$=
+*/
+#include <bits/stdc++.h>
+using namespace std;
+#define Fast                      \
+  {                               \
+    ios_base::sync_with_stdio(0); \
+    cin.tie(0);                   \
+    cout.tie(0);                  \
+  }
+#define ll long long
+vector<string> Act = {"Swimming", "Running", "Football"};
+int n, countt = 0;
+void sol(int day, vector<string> &B, string prev)
+{
+  if (day == n)
+  {
+    ++countt;
+    for (int i = 0; i < n; i++)
+    {
+      cout << B[i] << (i == n - 1 ? "\n" : " ");
+    }
+    return;
+  }
+
+  for (string A : Act)
+  {
+    if (A != prev)
+    {
+      B.push_back(A);
+      sol(day + 1, B, A);
+      B.pop_back();
+    }
+  }
+}
+signed main()
+{
+  Fast int t = 1;
+  // cin >> t;
+  while (t--)
+  {
+    cin >> n;
+    vector<string> arr;
+    sol(0, arr, "");
+    cout << "COUNT: " << countt << '\n';
+  }
+}
